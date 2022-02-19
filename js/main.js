8d9e8bed5f4aa07e775a159f7d29ecea29a958a4
Zohid-Jurakulovich <zohidjurakulovich@gmail.com>
2022-02-19 15:35:32 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let name = prompt("Ismingizni kiriting");
+console.log(name);
+
+let breakpoint = prompt("Boootstrap breakpointsda lg xossasi qancha pixelgacha amal qiladi?");
+console.log(breakpoint);
+
+let container = prompt("Container-fluid xossasi haqida ma'lumot bering");
+console.log(container);
+
+let typography = prompt("Typography nima?");
+console.log(typography);
+
+let gutters = prompt("gutters nima?")
+console.log(gutters);
+
+let control = prompt("Input va text area uchun style berishda qaysi classdan foydalaniladi?");
+console.log(control);
+
+let checkbox = prompt("Checkbox va radio yaratish uchun ularning input elementiga qanday class beriladi?")
+console.log(checkbox);
+
+let isKnow = prompt("checkbox yoki radio elementlarini yonma-yon joylashtirishni bilasizmi?");
+console.log(isKnow);
+
+let range = prompt("Range elementini sozlash uchun qaysi attributlardan foydalaniladi");
+console.log(range);
+
+let work = prompt("Input-group classi nima uchun ishlatilinadi");
+console.log(work);
+
+let badgeAlert = prompt("Alert va badge xossalarining farni nimada?")
+console.log(badgeAlert);
